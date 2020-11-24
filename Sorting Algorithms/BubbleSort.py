02cef1a2da66663ad0abc6fe89dd2c93213811c9
pioyi <42544934+pioyi@users.noreply.github.com>
2020-11-24 19:25:15 +0200

    Update BubbleSort.py

--- Sorting Algorithms/BubbleSort.py
+++ Sorting Algorithms/BubbleSort.py
@@ -2,7 +2,7 @@ def BubbleSort(seq):
     for i in range(len(seq)):
         for j in range(i+1, len(seq)):
 
-            # Compare the pair and swap them if the first one is greater
+            # Swap the pair if necessary
             if seq[i] > seq[j]:
                 seq[i], seq[j] = seq[j], seq[i] 
                 
